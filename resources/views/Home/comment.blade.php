2e29ca0cb613fb61642ae59d7bfefd74baf4acca
yoyooy <yoyooy>
2020-05-29 09:40:15 +0200

    fin du front

--- resources/views/Home/comment.blade.php
+++ resources/views/Home/comment.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 
-
+    <div class="container-comment">
+        <div class="first-step"><p>1</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+        <div class="second-step"><p>2</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+        <div class="third-step"><p>3</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+    </div>
 
 @endsection
