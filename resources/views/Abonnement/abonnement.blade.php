1cf84cdc67e3bc4afc90b088d4529880c53c448c
yoyooy <yoyooy>
2020-05-29 14:23:17 +0200

    vrai fin du front

--- resources/views/Abonnement/abonnement.blade.php
+++ resources/views/Abonnement/abonnement.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="abo">
     <h1>S'abonner</h1>
 
     <div>{{ Auth::user()->name}}</div>
@@ -9,19 +9,19 @@
     @forelse($abonnement as $a)
             <p>{{$a->nb_personne}} personne</p>
             <p>{{$a->nb_repas}} repas</p>
-            <a href="/choix/create/{{ Auth::user()->id}}">Choisir mes repas</a>
+            <a href="/choix/create/{{ Auth::user()->id}}" class="dropdown-item">Choisir mes repas</a>
 
             <p>Il y a encore {{$a->nb_repas}} repas dans votre abonnement! </p>
     @empty
         <p>Vous n'avez pas d'abonnement</p>
-        <a href="/ajoutAbonnement/create">s'abonner</a>
+        <a href="/ajoutAbonnement/create" class="dropdown-item">s'abonner</a>
     @endforelse
 
-  
+
 
 
     <form action="/ajoutAbonnement" class="form-group">
 
     </form>
-
+</div>
 @endsection
