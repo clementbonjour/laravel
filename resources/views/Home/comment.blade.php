05e51edefd26704b3cc3fc289337800a564041bc
yoyooy <yoyooy>
2020-05-29 09:54:46 +0200

    vrai fin du front

--- resources/views/Home/comment.blade.php
+++ resources/views/Home/comment.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
     <div class="container-comment">
-        <div class="first-step"><p>1</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
-        <div class="second-step"><p>2</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
-        <div class="third-step"><p>3</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+        <div class="first-step"><p>1</p><p>JE PERSONNALISE MON PANIER</p></div>
+        <div class="second-step"><p>2</p><p>JE REÇOIS MON PANIER CHEZ MOI</p></div>
+        <div class="third-step"><p>3</p><p>JE CUISINE DE BONS PETITS PLATS</p></div>
     </div>
 
 @endsection
